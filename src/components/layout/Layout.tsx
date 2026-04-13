231fbf93131a6ea087eef71000eca7447775a264
MayaZiv-Law <maya@mayaziv-law.co.il>
2026-04-13 13:29:25 +0300

    feat: integrate cookie consent banner into Layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode, useEffect } from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import CookieConsent from '@/components/CookieConsent';
 import { useLanguage } from '@/contexts/LanguageContext';
 import { cn } from '@/lib/utils';
 
@@ -58,6 +59,7 @@ const Layout = ({ children }: LayoutProps) => {
       <Header />
       <main id="main-content" className="flex-1 pt-20">{children}</main>
       <Footer />
+      <CookieConsent />
     </div>
   );
 };
